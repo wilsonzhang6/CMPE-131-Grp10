382a38bd9b85ff2add2b69c40e1e82b9d568b119
Carlos Rojas <carlos.rojas@sjsu.edu>
2019-09-16 04:59:16 -0700

    Change 'url/' to url_for('url/')
    url_for does a look up on the view function
    
    update gitignore pychache

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -25,5 +25,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
