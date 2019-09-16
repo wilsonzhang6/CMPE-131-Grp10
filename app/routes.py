fc72201d760013447d8aecfc6d1e1a5b254bce90
Carlos Rojas <carlos.rojas@sjsu.edu>
2019-09-16 03:49:32 -0700

    Add template for-loop index.html
    Delete __pycache__

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
 from flask import render_template
 from app import app
 
-# different URL the app will implement
-@app.route("/")
-# called view function
-def hello():
+@app.route('/')
+@app.route('/index')
+def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
