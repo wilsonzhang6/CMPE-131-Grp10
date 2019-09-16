e01399dae4a0199da5dd50ee7756dfba86095d52
Carlos Rojas <carlos.rojas@sjsu.edu>
2019-09-16 04:18:14 -0700

    Add forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+	
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
