7a1d7881991a8aa341c3cbdeeebbc8b64152d20c
Carlos Rojas <carlos.rojas@sjsu.edu>
2019-09-18 02:35:27 -0700

    Add database and models

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+
+from app import routes, models
