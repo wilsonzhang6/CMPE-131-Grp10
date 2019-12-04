1873e8dbb026c2a26f8da9bc3f072b5374f7874e
Visva <vis.suthar@yahoo.com>
2019-12-03 19:16:05 -0800

    test files

---
+++ app/tests/test_example.py
@@ -0,0 +1,19 @@
+import pytest
+
+from config import Config
+from flask import Flask
+from app import routes, models
+
+@pytest.fixture
+def app():
+   app = Flask(__name__)
+   return app
+
+@app.route('/')
+def index():
+    return app.response_class('OK')
+'''
+def test_example(client):
+    response = client.get("/")
+    assert response.status_code == 200
+'''
