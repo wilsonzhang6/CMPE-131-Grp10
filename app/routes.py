fdc6378bbafd5c6e5fb87fced17bd4a0058b39a3
Visva <vis.suthar@yahoo.com>
2019-12-04 14:41:50 -0800

    import RequestResetForm/ResetPasswordForm

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 #routes URL
 from flask import render_template, flash, redirect, url_for, request, abort
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, CreateRoutineForm, UpdateAccountForm
+from app.forms import LoginForm, RegistrationForm, CreateRoutineForm, UpdateAccountForm, RequestResetForm, ResetPasswordForm
 from app.models import User, Routine
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
