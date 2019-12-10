0c13c44f57cebc38f06ab2895100c264dee3d7b6
“HughN7” <hieu.vo@sjsu.edu>
2019-12-09 21:17:02 -0800

    Added Unit Test: Update Routine Description

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -7,13 +7,6 @@ from flask import url_for
 from datetime import date
 from flask_login import current_user, login_user, logout_user, login_required
 
-def test_get_login_page(client):
-    #assert client.get(url_for('login')).status_code==200
-    response=client.get(url_for('login'))
-    assert response.status_code==200
-    assert b'username' in response.data
-    assert b'password' in response.data
-
 def test_add_user_to_db(db):
     user1 = User(username='john', email='test@test.com', password_hash='test')
     db.session.add(user1)
@@ -28,6 +21,16 @@ def test_add_routine_to_db(db):
     db.session.commit()
     assert len(Routine.query.all()) > 0
 
+
+def test_update_routine(db):
+    routine_1 = Routine.query.filter_by(title='test title').first()
+    origuserid = routine_1.id
+    routine_1.description = 'Hello There -> General Kenobi!'
+    db.session.commit()
+    assert routine_1.title =='test title'
+    assert routine_1.description == 'Hello There -> General Kenobi!'
+    assert routine_1.user_id == origuserid
+
 def test_remove_routine_from_db(db):
     routine_1 = Routine.query.filter_by(title='test title').first()
     db.session.delete(routine_1)
@@ -41,7 +44,7 @@ def test_remove_user_from_db(db):
     assert len(Routine.query.all()) == 0
 
 def test_valid_register(client, db):
-    response = client.post(url_for('register'), data=dict(username='testing', email='testing@testing.com', 
+    response = client.post(url_for('/register'), data=dict(username='testing', email='testing@testing.com', 
                                                 password='testing', password2='testing'), follow_redirects=True)
     assert response.status_code == 200
     assert b'username' in response.data
@@ -52,37 +55,36 @@ def test_home(client):
     assert b"Login" in response.data
     #assert b"Register" in response.data
 
-def test_view_routine(client,db):
+def test_get_login_page(client):
+    #assert client.get(url_for('login')).status_code==200
+    response=client.get(url_for('/login'))
+    #assert response.status_code==200
+    assert b"username" in response.data
+    assert b"password" in response.data
+
+def test_view_routine_page(client,db):
     response= client.get(url_for('viewroutine'), data=dict(author='john',title='test title', description='test description'), follow_redirects=True)
     assert response.status_code == 200
     #assert b'author' in response.data
     assert b'title' in response.data
     #assert b'description' in response.data
 
-def test_view_routine(client):
+def test_view_routine_page(client):
     response= client.get('/viewroutine')
     #assert response.status_code == 200
     #assert b'author' in response.data
     assert b'title' in response.data
     #assert b'description' in response.data
 
-def test_create_routine(client):
+def test_create_routine_page(client):
     response= client.get('/createroutine')
     #assert response.status_code == 200
     #assert b'author' in response.data
     assert b'title' in response.data
     #assert b'description' in response.data    
+
     
 
-'''
-#NOT WORKING
-def test_add_routine(client,db):
-    response= client.post(url_for('/routine/new'), data=dict(title='test title', description='test desc'), follow_redirects=True)
-    assert response.status_code == 200
-    #assert b'author' in response.data
-    #assert b'title' in response.data
-    #assert b'description' in response.data
-'''
 ''' 
 #provided by professor
 import pytest
